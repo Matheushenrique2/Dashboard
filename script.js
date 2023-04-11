517fbed8a9771f164385e63a68dded8a1e0ec5d6
Matheushenrique2 <108466538+Matheushenrique2@users.noreply.github.com>
2023-04-11 17:28:37 -0300

    src

--- script.js
+++ script.js
@@ -2,34 +2,6 @@ document.querySelector(".jsFilter").addEventListener("click", function () {
   document.querySelector(".filter-menu").classList.toggle("active");
 });
 
-const content = document.querySelector(".content");
-const inputSearch = document.querySelector("input[type='search']");
-let items = [];
-
-inputSearch.oninput = () => {
-  content.innerHTML = "";
-
-  items
-  .filter((item) =>
-  item.toLowerCase().includes(inputSearch.value.toLowerCase())
-  )
-  .forEach((item) => addHTML (item))
-}
-function addHTML(item) {
-  const div = document.createElement("div");
-  div.innerHTML = item;
-  content.append(div);
-}
-
-fetch("")
-.then((data) => data.json())
-.then((users) => {
-  users.forEach((user) => {
-    addHTML(user.name);
-    items.push(user.name);
-    
-  });
-})
 document.querySelector(".grid").addEventListener("click", function () {
   document.querySelector(".list").classList.remove("active");
   document.querySelector(".grid").classList.add("active");
